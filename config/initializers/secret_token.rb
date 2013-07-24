08421cda39994094c55c5a210ec095e432297618
travhoang77 <travhoang@gmail.com>
2013-07-23 21:12:01 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sassygizmo::Application.config.secret_token = '1dbb056f1fd43ad7f5559663cf4cbc0c6e43a2ead1d0f6d0887dd3d2b2e6098dd76e0e6ac6317631a9b83b4df06bc619a4e479783b88fbf222f751c0ffd14401'
